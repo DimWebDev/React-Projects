90b0bb4cdd5a9c44998b3f97390fa506d6b43568
DimWebDev <77544032+DimWebDev@users.noreply.github.com>
2021-08-03 11:09:15 +0300

    Update unsplash.js

--- ImageSearch/src/api/unsplash.js
+++ ImageSearch/src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID 1eCNV_wIi_6nQb8BiHBi_0qqEH6jF4KwKlRMx7dZeP4'
+        Authorization: 'Client-ID Access Key'
     }
 })
 // The create method is going to create an instance of the axios 
